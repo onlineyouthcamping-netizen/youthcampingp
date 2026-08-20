e761691f8a91334a27749bab5e79840a26ac1181
parthyouthcamping-ops <onlineyouthcamping@gmail.com>
2026-08-20 15:06:14 +0530

    chore: remove legacy scratch scripts and obsolete markdown logs

--- backend/scripts/check_pages.js
+++
@@ -1,9 +0,0 @@
-const { prisma } = require('./src/lib/prisma');
-
-async function check() {
-  const pages = await prisma.pageBuilder.findMany();
-  console.log(JSON.stringify(pages, null, 2));
-  await prisma.$disconnect();
-}
-
-check();
--- backend/scripts/test_gemini.js
+++
@@ -1,1 +0,0 @@
-console.log('GEMINI_API_KEY exists:', !!process.env.GEMINI_API_KEY);
